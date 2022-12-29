a5eb2a2d98b2441b130cdd607b921f24c0505652
cbaciliodev <dbacilio88@outlook.es>
2022-12-29 08:30:47 -0500

    create constants
    create constants

---
+++ java-constants/Constants.java
@@ -0,0 +1,23 @@
+
+public class Constants {
+
+    public static final String DATA = "DATA";
+    public static final int NUMBER = 10;
+    public static final String FACTORY = DATA + " FACTORY";
+    public static final double DOUBLE = 1.0;
+    public static final float FLOTANT = 1.0f;
+    public static final long LARGE = 1234567890;
+
+
+    public static void main(String[] args) {
+
+        System.out.println(DATA);
+        System.out.println(FACTORY);
+        System.out.println(NUMBER);
+        System.out.println(DOUBLE);
+        System.out.println(FLOTANT);
+        System.out.println(LARGE);
+
+    }
+
+}
